7202d9fb536f29a30b3a3672736fc6e71012aebe
Sampreet-G <ghosh.sampreet@gmail.com>
2026-04-04 17:24:09 +0530

    Connected frontend with backend deployment

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000'
+  baseURL:
+    import.meta.env.MODE === 'development'
+      ? 'http://127.0.0.1:8000'
+      : 'https://ai-resume-analyzer-3-45w2.onrender.com'
 })
 
 export default API
