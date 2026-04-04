0f9cd0e4cd4712b63e566692c3f0cfc4ea185e34
Sampreet-G <ghosh.sampreet@gmail.com>
2026-04-04 17:34:23 +0530

    Connected backend with frontend deployment 4

--- Backend/main.py
+++ Backend/main.py
@@ -6,13 +6,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-                   "https://ai-resume-analyzer-two-nu.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(resume_router)
 
 @app.get("/")
--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL:
-    import.meta.env.MODE === 'development'
-      ? 'http://127.0.0.1:8000'
-      : 'https://ai-resume-analyzer-3-45w2.onrender.com'
+  baseURL: 'https://ai-resume-analyzer-3-45w2.onrender.com'
 })
 
 export default API
